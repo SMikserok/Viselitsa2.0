a34fcd1acb45fc306f7f4934910629ee1bb7b12a
Sergey Myshev <sergeymyshev95@gmail.com>
2025-01-16 19:16:41 +0500

    Заголовочный файл

---
+++ Visel.h
@@ -0,0 +1,7 @@
+void IgraKomp();
+void IgraIgrok();
+void Ris1(int& promah);
+void Ris2(int& promah);
+void Ris3(int& promah);
+bool isOnlyLetters(std::string& slovo);
+int IndByk(std::string& slovo);
